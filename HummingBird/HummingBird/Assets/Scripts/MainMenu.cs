61078095ba478556a6139df0856694bca5f6e8b0
masif040 <fa19-rcs-040@student.comsats.edu.pk>
2020-07-16 12:14:57 +0500

    Humming Bird and penguin
    Humming Bird and penguin

---
+++ HummingBird/HummingBird/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+  public void PlayPenguinGame()
+    {
+        SceneManager.LoadScene("Penguins");
+    } 
+
+
+  public void PlayHummingbirdsGame()
+    {
+        SceneManager.LoadScene("FlowerIsland");
+
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
